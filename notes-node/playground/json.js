ab4ff032f185a5457630db6595080607a97d8b11
Michael Bharrat <mbharrat@gmail.com>
2019-01-29 07:54:20 -0500

    Finished JSON example, TODO: NOTES

---
+++ notes-node/playground/json.js
@@ -0,0 +1,27 @@
+// var obj = {
+// 	name: 'Michael'
+// }
+
+// var stringObj = JSON.stringify(obj)
+// console.log(typeof stringObj)
+// console.log(stringObj)
+
+// var personString = '{"name": "Michael", "age":25}'
+// var person = JSON.parse(personString)
+// console.log(typeof person)
+// console.log(person)
+
+const fs = require('fs')
+
+var originalNote = {
+	title: 'Some title',
+	body: 'Some body'
+}
+// originalNoteString
+const originalNoteString = JSON.stringify(originalNote)
+fs.writeFileSync('notes.json',originalNoteString)
+
+var noteString = fs.readFileSync('notes.json')
+ note = JSON.parse(noteString)
+console.log(typeof note)
+console.log(note.title)
